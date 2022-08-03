bd6e3751521fefddccec7b7482f5e8ec28ddaaae
Geoffrey Ahlberg <gahlberg@diligent-networks.com>
2022-08-02 20:29:49 -0400

    Hub2 Creation

---
+++ aws/7.0/Hub2/output.tf
@@ -0,0 +1,15 @@
+
+output "FGTPublicIP1" {
+  value = aws_eip.FGTPublicIP1.public_ip
+}
+output "FGTPublicIP2" {
+  value = aws_eip.FGTPublicIP2.public_ip
+}
+output "Username" {
+  value = "admin"
+}
+
+output "Password" {
+  value = aws_instance.fgtvm-hub1.id
+}
+
