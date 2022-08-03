92329ffd844abce387739f9cef24f4c290e756e0
Geoffrey Ahlberg <gahlberg@diligent-networks.com>
2022-08-03 18:28:21 -0400

    chg ISP2 public allow

--- aws/7.0/Spoke1/fgtvm-spoke1.tf
+++ aws/7.0/Spoke1/fgtvm-spoke1.tf
@@ -25,7 +25,7 @@ resource "aws_network_interface_sg_attachment" "publicattachment1" {
 
 resource "aws_network_interface_sg_attachment" "publicattachment2" {
   depends_on           = [aws_network_interface.eth1]
-  security_group_id    = aws_security_group.allow_all.id
+  security_group_id    = aws_security_group.public_allow.id
   network_interface_id = aws_network_interface.eth1.id
 }
 
